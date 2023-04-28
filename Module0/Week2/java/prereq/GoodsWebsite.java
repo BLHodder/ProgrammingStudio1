54abce0f8d90170417de2e40547e4d9dcfa329ec
Ben Hodder <benhodds6@gmail.com>
2023-04-28 16:08:42 +1000

    some html stuff

--- Module0/Week2/java/prereq/GoodsWebsite.java
+++ Module0/Week2/java/prereq/GoodsWebsite.java
@@ -1,5 +1,7 @@
 package prereq;
 
+import java.util.Scanner;
+
 public class GoodsWebsite {
 
    public static void main(String[] args) {
@@ -8,8 +10,40 @@ public class GoodsWebsite {
       Listing item2 = new Listing();
       Listing item3 = new Listing();
 
+      Scanner userIn = new Scanner(System.in);
+      String title, description;
+      double price; 
+
       // TODO: Create three listings, setting the properties
+
+      title = userIn.next();
+      price = userIn.nextDouble();
+      description = userIn.next();
+
+      item1.setTitle(title);
+      item1.setPrice(price);
+      item1.setDescription(description);
+
+      title = userIn.next();
+      price = userIn.nextDouble();
+      description = userIn.next();
+
+      item2.setTitle(title);
+      item2.setPrice(price);
+      item2.setDescription(description);
+
+      title = userIn.next();
+      price = userIn.nextDouble();
+      description = userIn.next();
+
+      item3.setTitle(title);
+      item3.setPrice(price);
+      item3.setDescription(description);
+      
       
       // TODO: Output all of the item
+      item1.printInfo();
+      item2.printInfo();
+      item3.printInfo();
    }
 }
--- Module0/Week2/java/prereq/Listing.java
+++ Module0/Week2/java/prereq/Listing.java
@@ -2,16 +2,42 @@ package prereq;
 
 public class Listing {
    // These are examples properties
-   private String title;
+   private String title, description;
    private double price;
+   
 
    // TODO: Add more properties
-
    // TODO: Add your setter/getter here
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public void setPrice(double price) {
+      this.price = price;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public double getPrice() {
+      return price;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   
    
    public void printInfo() {
       // TODO: Complete this to print out the information of the Listing
       System.out.println("Item: " + title);
-      System.out.println("\tPrice: " + price);
+      System.out.println("Price: " + price);
+      System.out.println("Description: " + description);
    }
 }
